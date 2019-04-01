a4afd9a98b6ff81c615583d342432d1ac3d30aff
jpanicheliz <jpani.cheliz@gmail.com>
2019-04-01 20:56:57 +0200

    Añadiendo el archivo hello

---
+++ convertidor_de_farenheit_a_celcius.py
@@ -0,0 +1,10 @@
+grados_farenheit = 0
+
+grados_farenheit = float(input("Cual es el valor de grados farenheit que quieres transformar a celcius:"))
+
+grados_celcius = (grados_farenheit - 32 / 1.8)
+
+print("Los grados celcius son {}".format(grados_celcius))
+
+
+
